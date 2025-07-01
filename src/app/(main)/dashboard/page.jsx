945c0559d2686811752950428094d32e84d20f86
devmalya99 <debmalyamazumdar99@gmail.com>
2025-07-01 18:02:58 +0530

    v4 implemented zod resolvers

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+"use client";
 
+import { useUser } from "@clerk/nextjs";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {usernameSchema} from "@/app/lib/validators"
+import { useForm } from "react-hook-form";
 const DashboardPage = () => {
-  return (
-    <div>
-      Dashboard Page
-    </div>
-  )
-}
+  const { user, isLoaded } = useUser();
 
-export default DashboardPage
+  
+
+  useForm({
+    resolver: zodResolver(usernameSchema),
+  });
+  return (
+    <div className="space-y-8">
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome, {user?.firstName}!</CardTitle>
+        </CardHeader>
+        {/* <CardContent>
+          {!loadingUpdates ? (
+            <div className="space-y-6 font-light">
+              <div>
+                {upcomingMeetings && upcomingMeetings?.length > 0 ? (
+                  <ul className="list-disc pl-5">
+                    {upcomingMeetings?.map((meeting) => (
+                      <li key={meeting.id}>
+                        {meeting.event.title} on{" "}
+                        {format(
+                          new Date(meeting.startTime),
+                          "MMM d, yyyy h:mm a"
+                        )}{" "}
+                        with {meeting.name}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No upcoming meetings</p>
+                )}
+              </div>
+            </div>
+          ) : (
+            <p>Loading updates...</p>
+          )}
+        </CardContent> */}
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Your Unique Link</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div>
+              <div className="flex gap-2 mb-4">
+                <span className="p-1">{window?.location.origin}</span>
+
+                <Input placeholder="username" />
+              </div>
+            </div>
+            <Button type="submit">Update UserName</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default DashboardPage;
