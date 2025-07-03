c475a15d2bfbc85f357b54851d3ddd6f4669e988
devmalya99 <debmalyamazumdar99@gmail.com>
2025-07-03 16:17:48 +0530

    v9 event functionality done

--- src/app/(main)/events/page.jsx
+++ src/app/(main)/events/page.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import EventCard from '@/components/EventCard'
 
-const Events = () => {
+import {getUserEvents} from "@/actions/events.js"
+
+const EventsPage = () => {
   return (
-    <div>
-      Events
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Events/>
+
+    </Suspense>
   )
 }
 
-export default Events
+export default EventsPage
+
+const Events=async()=>{
+  const {events,username}= await getUserEvents();
+  console.log(events)
+  if(events.length===0)
+  {
+    return <p>You havnt created any events yet</p>
+  }
+
+  return <div>
+    {
+      events.map((event)=>{
+        return (
+        <EventCard 
+        key={event.id}
+        event={event}
+        username={username}
+        />)
+      })
+    }
+  </div>
+
+  
+}
